5003722fbba1ccbcd486a7bdb778f05db44907c0
Carl Barringer <cbarringer1@gmail.com>
2019-10-25 06:38:15 -0700

    added tables

--- SQLStudentExercises.sql
+++ SQLStudentExercises.sql
@@ -1,1 +1,27 @@
 ﻿--This is StudentExercises
+CREATE TABLE Students (
+	fname varchaR(15),
+	lname varchar(15),
+	slackHandle varchar(25),
+	cohort varchar(20),
+	exercises int(10),
+)
+
+CREATE TABLE Cohort (
+	cohortName varchar(25),
+	students int(10),
+	instructors int(10)
+)
+
+CREATE TABLE Instructor (
+	fname varchar(15),
+	lname varchar(15),
+	slackHandle varchar(25),
+	cohort varchar(20),
+	specialty varchar(25),
+)
+
+CREATE TABLE Exercises (
+	name varchar(25),
+	language varchar(25)
+)
